d9f61c7658a92cd1c901bbbedf38e3295eca9438
rushayhays <rushayhays@outlook.com>
2022-05-23 13:12:56 -0500

    phase 2 complete

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 
-//Phase 1
-//Display a message to the user asking them to guess the secret number.
-//Display a prompt for the user's guess.
-//Take the user's guess as input and save it as a variable.
-//Display the user's guess back to the screen.
+//Phase 2
+//Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
+//No longer display the user's guess. They know what they guessed, right?
+//Compare the user's guess with the secret number. Display a success message if the guess is correct, otherwise display a failure message.
 
 namespace GuessingGame
 {
@@ -12,9 +11,19 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            int SecretNumber = 42;
             Console.WriteLine("Guess the Secret Number:");
             string Guess = Console.ReadLine();
-            Console.WriteLine(Guess);
+            int GuessAsANumber = int.Parse(Guess);
+            if(SecretNumber == GuessAsANumber)
+            {
+                Console.WriteLine("Success you guessed the secret number!");
+            }
+            else
+            {
+                Console.WriteLine("Try Again, you failure.");
+            }
+
         }
     }
 }
