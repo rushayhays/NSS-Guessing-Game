d7b147053505f595c4acf636ea5556c969c26a14
rushayhays <rushayhays@outlook.com>
2022-05-23 14:24:11 -0500

    phase 7 completed

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 
-//Phase 6
+//Phase 7
 //The program should be updated to...
 
-//Inform the user if their guess was too high or too low, when they guess incorrectly.
+//Prompt the user for a difficulty level before they are prompted to guess the number.
+//The difficulty level should determine how many guesses the user gets. The difficulty levels should be:
+//Easy - this gives the user eight guesses.
+//Medium - this gives the user six guesses.
+//Hard - this gives the user four guesses.
 
 namespace GuessingGame
 {
@@ -14,9 +18,30 @@ namespace GuessingGame
 
 
            
-           
+           int SetDifficulty()
+           {
+                int Difficulty = 0;
+                Console.WriteLine("How difficult do you want this game to be?");
+                Console.WriteLine("1) Easy");
+                Console.WriteLine("1) Medium");
+                Console.WriteLine("1) Hard");
+                string userResponse = Console.ReadLine();
+                int userResponseAsANumber = int.Parse(userResponse);
+                if(userResponseAsANumber == 1){
+                    Difficulty = 8;
+                }
+                else if(userResponseAsANumber == 2){
+                    Difficulty = 6;
+                }
+                else if(userResponseAsANumber == 3){
+                    Difficulty = 4;
+                }else{
+                    Console.WriteLine("That is not a valid option");
+                }
+                return Difficulty;
+           }
 
-            void GuessingGame()
+            void GuessingGame(int DifficultyNumber)
             {
                 int GameCounter = 1;
                 Random r = new Random();
@@ -26,18 +51,20 @@ namespace GuessingGame
                 string Guess = Console.ReadLine();
                 int GuessAsANumber = int.Parse(Guess);
 
-                while(GameCounter != 4)
+                while(GameCounter != DifficultyNumber)
                 {
                     if(RandomSecretNumber == GuessAsANumber)
                     {
                         Console.WriteLine("Success you guessed the secret number!");
-                        GameCounter= 4;
+                        GameCounter= DifficultyNumber;
                         Console.WriteLine($"You finished in {GameCounter} guesses");
                     }
                     else
                     {
                         Console.WriteLine("Try Again, you failure.");
-                        Console.WriteLine($"You have made {GameCounter} guesses out of 4");
+                        Console.WriteLine("");
+                        Console.WriteLine($"You have made {GameCounter} guesses out of {DifficultyNumber}");
+                        Console.WriteLine("");
                         GameCounter++;
                         if(GuessAsANumber > RandomSecretNumber){
                             Console.WriteLine("Hint, your guess was too high");
@@ -46,6 +73,7 @@ namespace GuessingGame
                         {
                             Console.WriteLine("Hint, your guess was too low");
                         }
+                        Console.WriteLine("");
                         Console.WriteLine("Guess the secret number:");
                         string newGuess = Console.ReadLine();
                         GuessAsANumber = int.Parse(newGuess);
@@ -53,7 +81,7 @@ namespace GuessingGame
                 }
                 
             }
-            GuessingGame();
+            GuessingGame(SetDifficulty());
 
         }
     }
